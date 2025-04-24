bfa98c26c6c58cdbc694ff22dcc23efa0aa80127
Victor Querino Martins <victor.querino.martins@gmail.com>
2025-04-24 09:47:20 -0300

    Att (IA): <C++>

---
+++ IA/alg_genetico.cpp
@@ -0,0 +1,10 @@
+// População inicial -> Fitness -> Seleção -> cruzamento e mutação = nova população -> critério de parada satisfeito ? /sim -> fitness -> retorna melhor solução/ não -> fitness -> seleção ...
+
+#include <iostream>
+
+using namespace std;
+
+void  iniciar(){
+
+
+}
